34442ec29d1b4a8da1045828bf3c95d4513eaa05
Adeseto Akinwe <adeakinwe@gmail.com>
2026-04-15 23:56:04 +0100

    auth request

---
+++ ESG.Api/DTOs/AuthRequestDTO.cs
@@ -0,0 +1,10 @@
+namespace ESG.Api.DTOs
+{
+    public class AuthRequestDTO
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
